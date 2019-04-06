5926a3fe105b49bd35fa4933f04582678c260c2e
Cameron <cameronjmaccabe@gmail.com>
2019-04-05 23:10:59 -0700

    created MySQL connection

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+// *********************************************************************************
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// *********************************************************************************
+
+// Dependencies
+const mysql = require("mysql");
+
+// creates mySQL connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Melomania1888",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+});
+
+// exports the connection for other files to use
+module.exports = connection;
